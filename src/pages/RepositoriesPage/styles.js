b8f449452a1875e9fcdf865d7705c2fd1a3c8bbe
Valter B. Freitas Jr <valter191@hotmail.com>
2023-04-26 23:08:31 -0300

    first commmit!

---
+++ src/pages/RepositoriesPage/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  min-height: 100vh;
+
+  @media screen and (max-wdith: ${(props) => props.theme.breakpoints.md}) {
+    flex-direction: column;
+  }
+`;
+
+export const Sidebar = styled.aside`
+  background: ${(props) => props.theme.colors.background};
+  min-width: 20rem;
+  min-height: 100vh;
+  overflow-y: hidden;
+`;
+
+export const Main = styled.section`
+  background: ${(props) => props.theme.colors.cpmta};
+  width: 100%;
+  flex: 100%;
+  padding: 40px;
+  overflow-y: hidden;
+
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.md}) {
+    height: 100%;
+  }
+
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.sm}) {
+    padding: 40px 20p;
+  }
+`;
