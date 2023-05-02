c1555ab44175132f3d1475e83cd11161e1654caa
Valter B. Freitas Jr <valter191@hotmail.com>
2023-05-02 13:24:06 -0300

    Component Profile

--- src/pages/RepositoriesPage/Profile/index.js
+++ src/pages/RepositoriesPage/Profile/index.js
@@ -1,33 +1,56 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md';
 
 import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
-const Profile = () => (
+const Profile = ({ user }) => (
   <Container>
     <Header>
-      <Avatar src="https://avatars.githubusercontent.com/u/55540536?v=4" />
-      <Login>devsamurai</Login>
-      <Name>Dev Samurai</Name>
+      <Avatar src={user.avatar_url} />
+      <Login>{user.login}</Login>
+      <Name>{user.name}</Name>
     </Header>
     <Inner>
       <Data>
         <MdGroup size={20} />
-        30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+        {user.followers}&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;
+        {user.following}&nbsp;<i>seguindo</i>
       </Data>
-      <Data>
-        <MdWork size={20} /> Dev Samurai
-      </Data>
-      <Data>
-        <MdLocationCity size={20} /> São José dos Campos
-      </Data>
-      <Data>
-        <MdLink size={20} />
-        <a href="https://devsamurai.com.br">devsamurai.com.br</a>
-      </Data>
+      {/* Validação da companhia */}
+      {user.company && (
+        <Data>
+          <MdWork size={20} /> {user.company}
+        </Data>
+      )}
+      {user.location && (
+        <Data>
+          <MdLocationCity size={20} /> {user.location}
+        </Data>
+      )}
+      {user.blog && (
+        <Data>
+          <MdLink size={20} />
+          <a href={`\\${user.blog}`}>{user.blog}</a>
+        </Data>
+      )}
     </Inner>
   </Container>
 );
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
 
 export default Profile;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -6,16 +6,29 @@ import Repositories from './Repositories';
 
 import { Container, Sidebar, Main } from './styles';
 
-const RepositoriesPage = () => (
-  <Container>
-    <Sidebar>
-      <Profile />
-      <Filter />
-    </Sidebar>
-    <Main>
-      <Repositories />
-    </Main>
-  </Container>
-);
+const RepositoriesPage = () => {
+  const user = {
+    login: 'valterbfreitasjr',
+    avatar_url: 'https://avatars.githubusercontent.com/u/97262966?v=4',
+    name: 'Valter B. Freitas Jr',
+    followers: 0,
+    following: 0,
+    company: null,
+    blog: 'https://devjuninho.com.br',
+    location: 'Santo Anastácio - SP',
+  };
+
+  return (
+    <Container>
+      <Sidebar>
+        <Profile user={user} />
+        <Filter />
+      </Sidebar>
+      <Main>
+        <Repositories />
+      </Main>
+    </Container>
+  );
+};
 
 export default RepositoriesPage;
