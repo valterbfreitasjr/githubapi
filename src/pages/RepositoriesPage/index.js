3dd6e038a4ae00e01ab8b9b8fef7c1f8f3f7c6dd
Valter B. Freitas Jr <97262966+valterbfreitasjr@users.noreply.github.com>
2023-05-04 23:06:20 -0300

    Componente Repositories

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -22,36 +22,42 @@ const RepositoriesPage = () => {
 
   const repositories = [
     {
+      id: '1',
       name: 'Repositorie 1',
       description: 'Descrição',
       html_url: 'https://devjuninho.com.br',
       language: 'JavaScript',
     },
     {
+      id: '2',
       name: 'Repositorie 2',
       description: 'Descrição',
       html_url: 'https://devjuninho.com.br',
       language: 'PHP',
     },
     {
+      id: '3',
       name: 'Repositorie 3',
       description: 'Descrição',
       html_url: 'https://devjuninho.com.br',
       language: 'C#',
     },
     {
+      id: '4',
       name: 'Repositorie 4',
       description: 'Descrição',
       html_url: 'https://devjuninho.com.br',
       language: 'Java',
     },
     {
+      id: '5',
       name: 'Repositorie 5',
       description: 'Descrição',
       html_url: 'https://devjuninho.com.br',
       language: 'Ruby',
     },
     {
+      id: '6',
       name: 'Repositorie 6',
       description: 'Descrição',
       html_url: 'https://devjuninho.com.br',
@@ -74,7 +80,7 @@ const RepositoriesPage = () => {
         <Filter languages={languages} />
       </Sidebar>
       <Main>
-        <Repositories />
+        <Repositories repositories={repositories} />
       </Main>
     </Container>
   );
